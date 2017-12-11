90c0199b2d2fad411719c4cf05c8a886ac6094be
degraeveha <degraeveha@DCI5CG72136X6.gsinet.be>
2017-12-11 17:13:04 +0100

    Merge origin/master
    Conflicts:
    	CaseEfinal/src/caseefinal/Individual.java

--- CaseEfinal/src/caseefinal/CaseEfinal.java
+++ CaseEfinal/src/caseefinal/CaseEfinal.java
@@ -6,6 +6,7 @@
 package caseefinal;
 
 
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -27,7 +28,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class CaseEfinal {
 
     static int[][] roster;
-    public static int benchmark;
+    public static int benchmark = 80000;
 
     /**
      * @param args the command line arguments
@@ -39,19 +40,21 @@ public class CaseEfinal {
         //Heuristic.generateRoster20Shifts();
         //roster = Heuristic.heuristic(d);
 //roster=Heuristic.generateRosterOneShiftPerDay20Shifts();
-        Heuristic.heuristic(d);
+        Heuristic.heuristicArray(d);
         //Print.printRoster(roster, "files/FormatDepA.xlsx");
 
         // Print.printRoster(roster, "files/FormatDepA.xlsx");
-        ArrayList<Individual> myPop = new ArrayList<>();
-        int generationCount = 0;
-        while (Algo.getFittest(myPop).getOrigFitness() < benchmark) { // cyclical roster pref
-            generationCount++;
-            System.out.println("Generation: " + generationCount + " Fittest: " + Algo.getFittest(mypop));
-            myPop = Algo.evolvePopulation(myPop);
-        }
-        roster = Algo.getFittest(myPop).getRoster();
-        Print.printRoster(roster, "files/FormatDepA.xlsx");
+//        ArrayList<Individual> myPop = Heuristic.populatePopulation(50);
+//        int generationCount = 0;
+//        while (Algo.getFittest(myPop).getOrigFitness() < benchmark) { // cyclical roster pref
+//            generationCount++;
+//
+//            System.out.println("Generation: " + generationCount + " Fittest: " + Algo.getFittest(myPop));
+//
+//            myPop = Algo.evolvePopulation(myPop);
+//        }
+//        roster = Algo.getFittest(myPop).getRoster();
+//        Print.printRoster(roster, "files/FormatDepA.xlsx");
     }
 
 }
